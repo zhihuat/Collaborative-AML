dac25951c0cbc926445770817589698bb069db19
zhihuat <zhihuat@zju.edu.cn>
2025-06-24 03:14:09 -0700

    bug fix

--- aml_prob.py
+++ aml_prob.py
@@ -1,21 +1,27 @@
 """
-Probability based
+Probability-based
 """
 import pandas as pd
 import networkx as nx
 import numpy as np
-from tqdm import *
+from tqdm import tqdm
 import random
-from utils import generate_hash_functions, AMLBFS, lsh, minhash_signature, BloomFilter, find_transactions, check_sim
+from utils import generate_hash_functions, AMLBFS, lsh, minhash_signature, BloomFilter
+from sklearn.metrics import precision_score, accuracy_score, recall_score, f1_score
+
 import argparse
-import pickle
+import time
 import os
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", type=str, default='100K-unbalance', help="dataset name")
+parser.add_argument("--data_path", type=str, default='/path/to/100K-balance', help="dataset name")
 parser.add_argument("--rows", type=int, default=5, help="#rows")
-parser.add_argument("--bands", type=int, default=60, help="#bands")
+parser.add_argument("--bands", type=int, default=100, help="#bands")
+parser.add_argument("--repeats", type=int, default=1, help="repeat experiments")
+parser.add_argument("--output_dir", type=str, default='results/prob', help="output dir")
+parser.add_argument("--bf_size", type=int, default=500000, help="size of bloom filter")
+
 args = parser.parse_args()
 
 
@@ -23,12 +29,13 @@ accounts = pd.read_csv(args.data_path + '/accounts.csv')
 transactions = pd.read_csv(args.data_path + '/transactions.csv')
 alerts = pd.read_csv(args.data_path + '/alert_accounts.csv')
 
+save_dir = args.output_dir
+os.makedirs(save_dir, exist_ok=True)
 
 # AML config
-repeats = 5
-
+repeats = args.repeats
 K = 2
-len_threshold = 7  # sets larger than len_threshold will be ignored
+len_threshold = 5  # sets smaller than len_threshold will be ignored
 seed = 42
 
 # MinHash LSH config
@@ -37,7 +44,8 @@ rows = args.rows
 num_hash_functions = bands * rows
 
 # Bloom Filter config
-bf_size = 3000000
+bf_size = args.bf_size # 3000000   # amlworld
+# bf_size = 500000 # amlsim
 bf_hash_num = 7
 
 
@@ -76,13 +84,14 @@ for repeat in range(repeats):
     hash_functions = generate_hash_functions(num_hash_functions)
 
     ###################### Set: Bank a ######################
+    time0 = time.time()
     a_forward = {}
     a_backward = {}
     a_forward_account = {}
     a_backward_account = {}
     
 
-    for index, node_id in tqdm(enumerate(bank_a)):
+    for index, node_id in tqdm(enumerate(bank_a), desc="Set discover for band A"):
         # print(index)
         forward_set, forward_set_account = AMLBFS(src2dst, acct_bank_dict, node_id, K, reverse=False)
         backward_set, backward_set_account = AMLBFS(dst2src, acct_bank_dict, node_id, K, reverse=True)
@@ -93,15 +102,18 @@ for repeat in range(repeats):
         if len(backward_set) >= len_threshold:
             a_backward[node_id] = list(backward_set)
             a_backward_account[node_id] = backward_set_account
+            
+    print(f"Time usage for set discovery in bank A: {time.time() - time0}")
 
     ###################### Set: bank b ######################
+    time0 = time.time()
     b_forward = {}
     b_backward = {}
     b_forward_account = {}
     b_backward_account = {}
 
     
-    for index, node_id in tqdm(enumerate(bank_b)):
+    for index, node_id in tqdm(enumerate(bank_b), desc="Set discover for band B"):
         forward_set, forward_set_account = AMLBFS(src2dst, acct_bank_dict, node_id, K, reverse=False)
         backward_set, backward_set_account = AMLBFS(dst2src, acct_bank_dict, node_id, K, reverse=True)
         if len(forward_set) >= len_threshold:
@@ -110,45 +122,61 @@ for repeat in range(repeats):
         if len(backward_set) >= len_threshold:
             b_backward[node_id] = list(backward_set)
             b_backward_account[node_id] = backward_set_account    
+            
+    print(f"Time usage to find sets for nodes in bank B: {time.time() - time0}")
     
     ###################### MinHash & BF: Bank a ######################
+    time0 = time.time()
     a_forward_minhash = {key: minhash_signature(values, hash_functions) for key, values in a_forward.items()}
     a_forward_lsh = {key: lsh(signature, bands, rows) for key, signature in a_forward_minhash.items()}
     a_backward_minhash = {key: minhash_signature(values, hash_functions) for key, values in a_backward.items()}
     a_backward_lsh = {key: lsh(signature, bands, rows) for key, signature in a_backward_minhash.items()}
 
+    print(f"Time usage for computing minhash in bank A: {time.time() - time0}")
+    
+    time0 = time.time()
     # BloomFilter a
     bf_a_forward = []
     bf_a_backward = []
     for band in range(bands):
         bf1 = BloomFilter(bf_size, bf_hash_num) # Initialize BloomFilter
         bf2 = BloomFilter(bf_size, bf_hash_num) 
-        for (bands_list_forward, bands_list_backward) in tqdm(zip(a_forward_lsh.values(), a_backward_lsh.values())):
-            bf1.add(bands_list_forward[band]) 
+        for bands_list_forward in a_forward_lsh.values():
+            bf1.add(bands_list_forward[band])
+        
+        for bands_list_backward in a_backward_lsh.values():
             bf2.add(bands_list_backward[band])
         bf_a_forward.append(bf1)
         bf_a_backward.append(bf2)
 
+    print(f"Time usage for inserting Bloom filters in bank A: {time.time() - time0}")
 
     ###################### MinHash & BF: Bank b ######################
+    time0 = time.time()
     b_forward_minhash = {key: minhash_signature(values, hash_functions) for key, values in b_forward.items()}
     b_forward_lsh = {key: lsh(signature, bands, rows) for key, signature in b_forward_minhash.items()}
     b_backward_minhash = {key: minhash_signature(values, hash_functions) for key, values in b_backward.items()}
     b_backward_lsh = {key: lsh(signature, bands, rows) for key, signature in b_backward_minhash.items()}
 
+    print(f"Time usage for computing minhash in bank B: {time.time() - time0}")
+    time0 = time.time()
     # BloomFilter b
     bf_b_forward = []
     bf_b_backward = []
     for band in range(bands):
         bf1 = BloomFilter(bf_size, bf_hash_num) # Initialize BloomFilter
         bf2 = BloomFilter(bf_size, bf_hash_num) 
-        for (bands_list_forward, bands_list_backward) in tqdm(zip(b_forward_lsh.values(), b_backward_lsh.values())):
+        for bands_list_forward in b_forward_lsh.values():
             bf1.add(bands_list_forward[band]) 
+        
+        for bands_list_backward in b_backward_lsh.values():
             bf2.add(bands_list_backward[band])
         bf_b_forward.append(bf1)
         bf_b_backward.append(bf2)
+    print(f"Time usage for inserting Bloom filters in bank B: {time.time() - time0}")
     
     ###################### Comparation ######################
+    time0 = time.time()
     roc_list_a_forward = np.zeros(len(accounts))
     roc_list_a_backward = np.zeros(len(accounts))
     roc_list_b_forward = np.zeros(len(accounts))
@@ -160,12 +188,10 @@ for repeat in range(repeats):
             roc_list_a_forward[idx] = 1
 
 
-    
     for idx, forward in b_forward_lsh.items():
         bf_lsh_match = any(bf_a_backward[k].contains(band) for (k, band) in enumerate(forward))
         if bf_lsh_match:
             roc_list_b_forward[idx] = 1
-
 
 
     for idx, backward in a_backward_lsh.items():
@@ -179,15 +205,21 @@ for repeat in range(repeats):
         bf_lsh_match = any(bf_a_forward[k].contains(band) for (k, band) in enumerate(backward))
         if bf_lsh_match:
             roc_list_b_backward[idx] = 1
+            
+    print(f"Time usage for membership testing: {time.time() - time0}")
 
-    
     roc_list = np.zeros(len(accounts))
     a_forward_idx = np.where(roc_list_a_forward == 1)[0]
     a_backward_idx = np.where(roc_list_a_backward == 1)[0]
     b_forward_idx = np.where(roc_list_b_forward == 1)[0]
     b_backward_idx = np.where(roc_list_b_backward == 1)[0]
     
-
+    roc_list[a_forward_idx] = 1
+    roc_list[a_backward_idx] = 1
+    roc_list[b_forward_idx] = 1
+    roc_list[b_backward_idx] = 1
+    
+    # find intermediate nodes using src and dst nodes
     for src in a_forward_idx:
         for dst in b_backward_idx:
             union_set = np.array(list(set(a_forward_account[src]) & set(b_backward_account[dst]))) # & for AMLWorld
@@ -199,10 +231,18 @@ for repeat in range(repeats):
             union_set = np.array(list(set(b_forward_account[src]) & set(a_backward_account[dst]))) # & for AMLWorld
             if len(union_set) > 0:
                 roc_list[union_set] = 1
-                
+                                
     infer_list = roc_list.tolist()
     
-
+    results.append({
+        'iteration': repeat,  # current iteration number
+        'acc': accuracy_score(ground_truth, infer_list),
+        'precision': precision_score(ground_truth, infer_list),
+        'recall': recall_score(ground_truth, infer_list),
+        'f1': f1_score(ground_truth, infer_list),
+    })
+        
+    
 results_df = pd.DataFrame(results)
 
 final_results = results_df.agg({
@@ -215,5 +255,5 @@ final_results = results_df.agg({
 
 final_results = final_results.round(4)
 print(final_results)
-save_path = 'results/prob/' + args.data_path.split('/')[-1].split('_')[0] + f'_thred{len_threshold}_rows{rows}_bands{bands}.csv'
+save_path = os.path.join(save_dir, args.data_path.split('/')[-1].split('_')[0] + f'_thred{len_threshold}_rows{rows}_bands{bands}.csv')
 final_results.to_csv(save_path, index=False)
